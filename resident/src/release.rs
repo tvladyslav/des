82c7725cd523554a20ac4555ac5891a02379cda5
Vladyslav Tsilytskyi <ykp@protonmail.ch>
2023-01-16 16:33:29 +0100

    Make configuration automatic

--- resident/src/release.rs
+++ resident/src/release.rs
@@ -8,10 +8,14 @@ use windows::{w, core::PCWSTR};
 // Get-FileHash -Algorithm SHA512 -LiteralPath target\release\des-stub.exe | Select-Object -ExpandProperty Hash
 pub const STUB_HASH: &str = "C47E23101074B4B52ED6C8C2EBE97AE4A6812C62D4AF62282AC8703360CF6EC8B37B8E5C7C004C66FD07CD3E9A36420C64CB7862E31B8B950E0E1C10EB1DEE82";
 
-// Debug
+// Please edit version in main.rs!
+
+#[cfg(debug_assertions)]
 pub const TRAY_ICON_PATH: PCWSTR = w!("resources/find_bug_icon_32px_by_Chenyu_Wang.ico");
+#[cfg(debug_assertions)]
 pub const HOME_FOLDER: &str = "./target/debug/";
 
-// Release
-// pub const TRAY_ICON_PATH: PCWSTR = w!("find_bug_icon_32px_by_Chenyu_Wang.ico");
-// pub const HOME_FOLDER: &str = "./";
+#[cfg(not(debug_assertions))]
+pub const TRAY_ICON_PATH: PCWSTR = w!("find_bug_icon_32px_by_Chenyu_Wang.ico");
+#[cfg(not(debug_assertions))]
+pub const HOME_FOLDER: &str = "./";
